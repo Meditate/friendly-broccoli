a52e283e0c02e36a17fdc599919e6d7ba5d96f91
meditate <aroundofyou@gmail.com>
2019-12-27 23:27:30 +0100

    init commit

---
+++ src/components/bandit.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>Bandit:</h3>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Bandit',
+    data() {
+      return {
+        cells: null,
+      };
+    },
+    created: function() {
+      axios
+       .get('https://jsonplaceholder.typicode.com/users')
+       .then(res => {
+         this.users = res.data;
+       })
+    }
+  }
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
